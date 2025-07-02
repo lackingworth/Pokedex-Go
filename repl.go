92946fffd68640d3cea13b66da2605d09655374d
lackingworth <kotanwot@gmail.com>
2025-07-02 21:37:06 +0300

    basic REPL block

--- repl.go
+++ repl.go
@@ -7,10 +7,40 @@ import (
 	"strings"
 )
 
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Provide usage manual",
+		callback:    commandHelp,
+	},
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	lowerCase := strings.ToLower(text)
 	splitString := strings.Fields(lowerCase)
 	return splitString
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex\n")
+	return nil
 }
 
 func startRepl() {
@@ -20,11 +50,26 @@ func startRepl() {
 		reader.Scan()
 
 		words := cleanInput(reader.Text())
+		command := words[0]
 
 		if len(words) == 0 {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", words[0])
+		switch command {
+		case commands["exit"].name:
+			err := commandExit()
+			if err != nil {
+				fmt.Println(err)
+			}
+		case commands["help"].name:
+			err := commandHelp()
+			if err != nil {
+				fmt.Println(err)
+			}
+		default:
+			fmt.Println("Unknown command: " + command)
+		}
+
 	}
 }
