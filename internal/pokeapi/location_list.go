f2acd664cd70ae156ba2e1ef65ff5dd99f8a0ac7
lackingworth <kotanwot@gmail.com>
2025-07-04 14:56:31 +0300

    Implemented basic caching system

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -2,6 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +10,16 @@ func (c *Client) ListLocations(pageURL *string) (LocationAreasResp, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
+	}
+
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := LocationAreasResp{}
+		err := json.Unmarshal([]byte(val), &locationResp)
+		if err != nil {
+			return LocationAreasResp{}, err
+		}
+
+		return locationResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -22,11 +33,17 @@ func (c *Client) ListLocations(pageURL *string) (LocationAreasResp, error) {
 	}
 	defer res.Body.Close()
 
-	var locationAreas LocationAreasResp
-	err = json.NewDecoder(res.Body).Decode(&locationAreas)
+	dat, err := io.ReadAll(res.Body)
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
 
+	var locationAreas LocationAreasResp
+	err = json.Unmarshal(dat, &locationAreas)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	c.cache.Add(url, dat)
 	return locationAreas, nil
 }
